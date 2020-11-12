2cf64f7ca9eab6239b10790ac2f3eb50fbbfb3d3
Yoojin Lee <yoojinLee.dev@gmail.com>
2020-11-12 15:57:26 +0900

    feat:Add DTO.class
    MVC 개념을 이해하기 위한 공부

---
+++ src/net/member/db/Memberbean.java
@@ -0,0 +1,41 @@
+package net.member.db;
+
+public class Memberbean {
+	private String id;
+	private String pw;
+	private String name;
+	private int age;
+	private String joinDate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(String joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+}
